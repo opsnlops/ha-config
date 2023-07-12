d3c43e880c9df107a4b285b4e486c85811aeace3
April White <april@opsnlops.io>
2023-07-11 18:55:46 -0700

    Set up the new Office Presence senor using mmWave
    This is based on code from:
    
    https://github.com/hjmcnew/esphome-hs2xx3a-custom-component
    
    It doesn't have a license attached, so I don't know what the author's
    intent was. If they let me know, I'll update this.

---
+++ esphome/custom_components/hs2xx3a/binary_sensor/hs2xx3a_binary_sensor.cpp
@@ -0,0 +1,29 @@
+/**
+
+ This is from:
+    https://github.com/hjmcnew/esphome-hs2xx3a-custom-component
+
+*/
+
+#include "esphome/core/log.h"
+#include "hs2xx3a_binary_sensor.h"
+
+namespace esphome {
+namespace hs2xx3a {
+
+static const char *TAG = "hs2xx3a.binary_sensor";
+
+void HS2XX3ABinarySensor::setup() {
+    
+}
+  
+void HS2XX3ABinarySensor::update() {
+
+}
+
+void HS2XX3ABinarySensor::dump_config() {
+    ESP_LOGCONFIG(TAG, "HS2XX3A binary sensor");
+}
+
+} //namespace hs2xx3a
+} //namespace esphome
---
+++ esphome/custom_components/hs2xx3a/hs2xx3a.cpp
@@ -0,0 +1,29 @@
+/**
+
+ This is from:
+    https://github.com/hjmcnew/esphome-hs2xx3a-custom-component
+
+*/
+
+#include "esphome/core/log.h"
+#include "hs2xx3a.h"
+
+namespace esphome {
+namespace hs2xx3a {
+
+static const char *TAG = "hs2xx3a.component";
+
+void HS2XX3A::setup() {
+
+}
+
+void HS2XX3A::loop() {
+
+}
+
+void HS2XX3A::dump_config(){
+    ESP_LOGCONFIG(TAG, "HS2xx3A component");
+}
+
+}  // namespace hs2xx3a
+}  // namespace esphome
